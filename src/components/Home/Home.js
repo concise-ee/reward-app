5604d47a054b6278a996c65b8caed9f36a75bc94
Nusaeb Nur Alam <nusaeb.alam@easypark.net>
2021-02-11 13:19:16 +0200

    first reward!

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Home.module.scss';
+
+const Home = () => (
+  <div className={styles.Home}>
+    Welcome to the REWARD Äpp!
+  </div>
+);
+
+Home.propTypes = {};
+
+Home.defaultProps = {};
+
+export default Home;
