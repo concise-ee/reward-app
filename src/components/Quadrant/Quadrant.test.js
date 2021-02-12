ad8b676260cd98a78c141094d348f35eb4a7ef1b
Nusaeb Nur Alam <nusaeb.alam@easypark.net>
2021-02-12 08:56:41 +0200

    deleted unnecessary files

--- src/components/Quadrant/Quadrant.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Quadrant from './Quadrant';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Quadrant />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
